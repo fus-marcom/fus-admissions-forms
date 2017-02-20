58fed77d05a472ee56ced7e62bb2a049e130277b
Jesse R Weigel <jesse@jesseweigel.com>
2017-02-20 14:04:54 -0500

    Add responsive-table class to event results table

--- events.js
+++ events.js
@@ -2,15 +2,19 @@ console.log('w007!!!1');
 
 // Use jQuery here
 jQuery( document ).ready(function( $ ) {
-    $('link[href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/themes/smoothness/jquery-ui.css"]').appendTo('head');
+  $('link[href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/themes/smoothness/jquery-ui.css"]').appendTo('head');
 
-    $('link[href="https://jesserweigel.github.io/fus-admissions-forms/events.css"]').appendTo('head');
+  $('link[href="https://jesserweigel.github.io/fus-admissions-forms/events.css"]').appendTo('head');
 
-    $('body').prepend(`
-      <nav>
-       <div class="nav-wrapper">
-         <a href="#" class="brand-logo center"><img class="responsive-img" src="https://franciscan.university/img/side-nav-logo.jpg" /></a>
-       </div>
-      </nav>
-      `);
-    });
+  $('body').prepend(`
+    <nav>
+     <div class="nav-wrapper">
+       <a href="#" class="brand-logo center"><img class="responsive-img" src="https://franciscan.university/img/side-nav-logo.jpg" /></a>
+     </div>
+    </nav>
+    `);
+
+  $('.resultTable').addClass('responsive-table');
+
+
+});
