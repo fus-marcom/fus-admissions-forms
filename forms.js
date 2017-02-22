54d8694dda8cad78e0f6ceddb9c0b77e4892f782
Jesse R Weigel <jesse@jesseweigel.com>
2017-02-22 09:16:12 -0500

    Add css and js files for inquiry form

---
+++ forms.js
@@ -0,0 +1,11 @@
+jQuery( document ).ready(function( $ ) {
+
+  $('body').prepend(`
+    <nav>
+     <div class="nav-wrapper">
+       <a href="#" class="brand-logo center"><img class="responsive-img" src="https://franciscan.university/img/side-nav-logo.jpg" /></a>
+     </div>
+    </nav>
+    `);
+
+});
